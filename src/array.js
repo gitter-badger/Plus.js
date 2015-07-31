9d653aa48aafe99fa2e7cc8eab837253266bab72
Luke Shimkus <super99leo@gmail.com>
2015-07-31 10:26:06 -0400

    Added useless first method

--- src/array.js
+++ src/array.js
@@ -71,6 +71,10 @@ Array.prototype.append = function(){
   }
 };
 
+Array.prototype.first = function(){
+    return this[0];
+};
+
 Array.prototype.last = function(){
   return this[this.length - 1];
 };
